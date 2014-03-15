3ebcc19c1c98bec13ae60c7fa80c4014c2f7f2a4
ryan_osten <ryan@classvantage.com>
2014-03-15 09:31:33 -0400

    final todo

--- todo.rb
+++ todo.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@todos = []
+@@todos = ["take out trash", "walk dog"]
 
 get '/' do
 	erb :todos, locals:{
 		todo_list: @@todos
 	}
 end
+
+post '/add' do
+	@@todos << params[:todo]
+	redirect to('/')
+end
+
+post '/remove/:i' do
+	@@todos.delete_at(params[:i].to_i)
+	redirect to('/')
+	end
+
